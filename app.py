8d5800327554b92bcd9d3ab9303d68a4fa698563
shweta <s.jagdale0604@gmail.com>
2025-06-16 15:21:14 +0530

    Initial commit for render

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
-msg = os.getenv("MSG", "Hello from Flask in Docker!")
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template("index.html",message=msg)
+    msg = os.getenv("MSG", "Default message from Flask")
+    return render_template("index.html", message=msg)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
